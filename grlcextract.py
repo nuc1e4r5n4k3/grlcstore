6834af7495f657111c824e906d4a5f9315d98fe6
Erik van `t Wout <erik@ewnetworks.eu>
2018-04-20 23:24:10 +0200

    Initial commit

---
+++ grlcextract.py
@@ -0,0 +1,80 @@
+import sys
+
+from binascii import hexlify, unhexlify
+from hashlib import sha1, sha256
+
+from jsonrpc.authproxy import AuthServiceProxy, JSONRPCException
+
+
+RPCUSER = 'MY_GARLICOIN_CORE_RPC_USERNAME'
+RPCPASSWORD = 'MY_GARLICOIN_CORE_RPC_PASSWORD'
+RPCPORT = 42068
+
+
+MAGIC = 'GRLCFILE'
+__LAST_ID__ = '0000000000000000000000000000000000000000000000000000000000000000'
+
+rpcproxy = None
+
+def get_rpc_proxy():
+    global rpcproxy
+    if rpcproxy == None:
+        rpcproxy = AuthServiceProxy('http://%s:%s@localhost:%d/' % (RPCUSER, RPCPASSWORD, RPCPORT))
+    return rpcproxy
+
+def get_tx_data_entries(txid):
+    proxy = get_rpc_proxy()
+    tx = proxy.decoderawtransaction(proxy.getrawtransaction(txid))
+    return [ unhexlify(output['scriptPubKey']['asm'].split(' ')[-1]) for output in filter(lambda output: output['scriptPubKey']['type'] == 'nulldata', tx['vout']) ]
+
+def load_chunks_ids_from_index_chain(txid):
+    data = ''
+    while txid != __LAST_ID__:
+        entries = get_tx_data_entries(txid)
+        txid = hexlify(entries[0])
+        data += ''.join(entries[1:])
+
+    if len(data) % 32 != 0:
+        raise Exception('Error decoding chunk IDs')
+
+    return [ hexlify(data[i:i+32]) for i in range(0, len(data), 32) ]
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: %s <master index txid>' % sys.argv[0])
+        sys.exit(1)
+
+    master_index = get_tx_data_entries(sys.argv[1])
+
+    if master_index[0] != MAGIC:
+        raise Exception('Invalid Master File Index transaction ID')
+
+    filename = master_index[1]
+    file_sha1 = hexlify(master_index[2])
+    file_sha256 = hexlify(master_index[3])
+    index_chain_txid = hexlify(master_index[4])
+
+    print("""
+    Extracting:    %s
+    SHA1 digest:   %s
+    SHA256 digest: %s
+    Index chain:   %s
+""" % (master_index[1], file_sha1, file_sha256, index_chain_txid))
+
+    print('Resolving indices...')
+    chunks = load_chunks_ids_from_index_chain(index_chain_txid)
+    print('File stored in %d chunks, extracting from chain...' % len(chunks))
+
+    data = ''.join([ ''.join(get_tx_data_entries(txid)) for txid in chunks ])
+    print('Extracted %d bytes' % len(data))
+
+    if sha1(data).hexdigest() != file_sha1:
+        raise Exception('Extracted file, but SHA1 digest does not match Master File Index')
+    if sha256(data).hexdigest() != file_sha256:
+        raise Exception('Extracted file, but SHA256 digest does not match Master File Index')
+
+    print('File data verification OK, writing out...')
+    with open(filename, 'wb') as f:
+        f.write(data)
+
+    print('Success!')
---
+++ grlcstore.py
@@ -0,0 +1,157 @@
+import sys
+
+from binascii import hexlify, unhexlify
+from hashlib import sha1, sha256
+from os import path
+
+from jsonrpc.authproxy import AuthServiceProxy, JSONRPCException
+
+
+RPCUSER = 'MY_GARLICOIN_CORE_RPC_USERNAME'
+RPCPASSWORD = 'MY_GARLICOIN_CORE_RPC_PASSWORD'
+RPCPORT = 42068
+
+
+MAGIC = 'GRLCFILE'
+
+BLOCKSIZE = 72
+CHUNKSIZE = BLOCKSIZE * 0xfb
+
+FEE = 0.001
+
+__LAST_ID__ = '0000000000000000000000000000000000000000000000000000000000000000'
+
+rpcproxy = None
+
+def get_rpc_proxy():
+    global rpcproxy
+    if rpcproxy == None:
+        rpcproxy = AuthServiceProxy('http://%s:%s@localhost:%d/' % (RPCUSER, RPCPASSWORD, RPCPORT))
+    return rpcproxy
+
+class Transaction(object):
+    VERSION = '02000000'
+    INPUTS = '00'
+    COINS_OUT = '0000000000000000'
+    OP_RETURN = '6a'
+    FOOTER = '00000000'
+
+    def __init__(self, data_outputs):
+        self.unsigned = None
+        self.signed = None
+        self.id = None
+        self.generate(data_outputs)
+
+    def generate(self, data_outputs):
+        if len(data_outputs) >= 0xfd:
+            outputs = 'fd' + '%02x%02x' % (len(data_outputs) % 256, len(data_outputs) / 256)
+        else:
+            outputs = '%02x' % len(data_outputs)
+
+        self.template = self.VERSION + self.INPUTS + outputs
+        for output in data_outputs:
+            data_len = len(output)
+            self.template += self.COINS_OUT
+            self.template += '%02X' % (data_len + 2)
+            self.template += self.OP_RETURN
+            self.template += '%02X' % data_len + hexlify(output)
+        self.template += self.FOOTER
+
+    def fund(self):
+        self.unsigned = get_rpc_proxy().fundrawtransaction(self.template)['hex']
+
+    def sign(self):
+        self.signed = get_rpc_proxy().signrawtransaction(self.unsigned)['hex']
+
+    def broadcast(self):
+        self.id = get_rpc_proxy().sendrawtransaction(self.signed)
+
+class Chunk(object):
+    def __init__(self, data=None):
+        self.outputs = []
+        self._tx = None
+        if data != None:
+            for block in [ data[i:i + BLOCKSIZE] for i in range(0, len(data), BLOCKSIZE) ]:
+                self.append(block)
+
+    def prepend(self, block):
+        self.outputs.insert(0, block)
+
+    def append(self, block):
+        self.outputs.append(block)
+
+    def blocks(self):
+        return self.outputs
+
+    def raw(self):
+        return ''.join(self.outputs)
+
+    def store(self):
+        if self._tx == None:
+            self._tx = Transaction(self.outputs)
+            try:
+                self._tx.fund()
+                self._tx.sign()
+                self._tx.broadcast()
+            except JSONRPCException as e:
+                print(e.error)
+                raise
+        return self._tx.id
+
+class Blob(object):
+    def __init__(self, data):
+        self.data = data
+        self._chunks = None
+
+    def as_chunks(self):
+        if self._chunks == None:
+            self._chunks = self.encode()
+        return self._chunks
+
+class ChainedBlob(Blob):
+    def encode(self):
+        return [ Chunk(self.data[i:i + CHUNKSIZE - BLOCKSIZE]) for i in range(0, len(self.data), CHUNKSIZE - BLOCKSIZE) ]
+
+    def store(self):
+        last_id = __LAST_ID__
+        for chunk in reversed(self.as_chunks()):
+            chunk.prepend(unhexlify(last_id))
+            last_id = chunk.store()
+        return last_id
+
+class IndexedBlob(Blob):
+    def encode(self):
+        return [ Chunk(self.data[i:i + CHUNKSIZE]) for i in range(0, len(self.data), CHUNKSIZE) ]
+
+    def store(self):
+        print('Writing out data, %d chunks at %d bytes per chunk...' % (len(self.as_chunks()), CHUNKSIZE))
+        chunk_ids = [ chunk.store() for chunk in self.as_chunks() ]
+        index_data = ChainedBlob(''.join([ unhexlify(id) for id in chunk_ids ]))
+        print('Writing out %d indexes...' % len(index_data.as_chunks()))
+        return index_data.store()
+
+class ChainFile(object):
+    def __init__(self, data, name='unnamed'):
+        self.blob = IndexedBlob(data)
+        self.name = name[:BLOCKSIZE]
+        self.hash20 = sha1(data).digest()
+        self.hash32 = sha256(data).digest()
+
+    def store(self):
+        first_index_chunk_id = self.blob.store()
+        chunk = Chunk()
+        chunk.append(MAGIC)
+        chunk.append(self.name)
+        chunk.append(self.hash20)
+        chunk.append(self.hash32)
+        chunk.append(unhexlify(first_index_chunk_id))
+        return chunk.store()
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: %s <file>' % sys.argv[0])
+        sys.exit(1)
+    with open(sys.argv[1], 'rb') as file:
+        blob = ChainFile(file.read(), name=sys.argv[1].split(path.sep)[-1])
+        txid = blob.store()
+        print('Stored on chain. File Master Index transaction ID: %s' % txid)
